1e45fba54b69ab9c24e8a01382d876136b0d5e43
Sichen Zhao <sichen@lnxsrv05.seas.ucla.edu>
2015-05-14 03:11:05 -0700

    merge2

--- debug.c
+++ debug.c
@@ -3,12 +3,12 @@
 bool debugMode(void)
 {
   char str[100];
-  printf("(debug) ");
+  fprintf(stderr, "(debug) ");
   scanf("%100s", str);
   while (strlen(str) != 1)
   {
     print_debugInfo("Invalid command. Type \"h\" for help");
-    printf("(debug) ");
+    fprintf(stderr, "(debug) ");
     scanf("%100s", str);
   }
   char op = str[0];
@@ -18,14 +18,37 @@ bool debugMode(void)
 	      print_debugInfo("Continue.");
 	      return false;
     case 'q':
-	      printf("(debug) Quit?(y/n) ");
+	      fprintf(stderr, "(debug) Quit?(y/n) ");
 	      scanf("%100s", str);
-	      while (strlen(str) != 1 || strlen)
+	      while (strlen(str) != 1 || (str[0]!='y' && str[0] != 'n'))
 	      {
 		print_debugInfo("Invalid command.");
-		printf("(debug) ");
+		fprintf(stderr, "(debug) Quit?(y/n)");
 		scanf("%100s", str);
-
+	      }
+	      if (str[0] == 'y')
+	      {
+		print_debugInfo("Quit.");
+		exit(0);
+	      }
+	      else
+	      {
+		print_debugInfo("Not confirmed.");
+	      }
+    case 'b':
+	      if (op == 'b')
+	      {
+		pid_t pid = fork();
+		if (pid == 0)
+		{
+		  char** cmd = {"bash"};
+		  execvp(cmd[0], cmd);
+		}
+		else
+		{
+		  int status;
+		  waitpid(pid, &status, 0);
+		}
 	      }
   }
   return true;
